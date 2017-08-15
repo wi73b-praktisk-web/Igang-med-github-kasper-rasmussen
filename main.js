7c1656f23803601ebfc91b19070751212a6ec3ad
KasperHovlandRasmussen <3803976@rts-365.dk>
2017-08-15 11:09:28 +0200

    test

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-console.log('halløj');
-console.log('halløj2');
+/*
+Øvelse 1:
+var navn = "Kasper";
+var alder = 16;
+console.log(`Mit navn er ${navn} og jeg er ${alder} år gammel.`);
+*/
+
+/*
+Øvelse 2: 
+var moms = "1.25";
+var pris_uden_moms = "400";
+var pris_med_moms = moms * pris_uden_moms;
+console.log(pris_med_moms);
+
+var pris_uden_moms_02 = moms / pris_med_moms;
+console.log(pris_uden_moms_02);
+*/
+
+/*
+Øvelse 3
+var forste = 10;
+var anden = 20;
+var backup = forste;
+forste = anden;
+anden = backup;
+console.log(forste + " " + anden);
+*/
